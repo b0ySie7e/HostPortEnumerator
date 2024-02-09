ece54947abbdc383526e7bac356f73f347e1dde3
b0ySie7e <zombi1532ao@gmail.com>
2024-02-08 21:44:03 -0500

    upfile script v1.0

---
+++ HostPortEnumerator.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+#192.168.98.5
+
+# Colors
+PBlack='\033[30m'		# 1
+PRed='\033[31m'		    # 2
+PGreen='\033[32m'		# 3
+POrange='\033[33m'		# 4
+PBlue='\033[34m'		# 5
+PPurple='\033[35m'		# 6
+PCyan='\033[36m'		# 7
+PGray='\033[37m'		# 8
+PWhite='\e[37m'		    # 9
+NC='\033[39m'
+
+
+function ctrl_c(){
+    printf "\n\n ${PRed}[*] Going out ...\n"
+    tput cnorm; exit 1
+}
+
+trap ctrl_c INT
+
+
+function def_help(){
+    printf "${PGreen}[*] The necesary arguments are:\n"
+    printf "\t-H  : Host scanning\n"
+    printf "\t-P  : Port scanning\n"
+    printf "\t Example: \n"
+    printf "\t$0 -H ${PCyan}10.10.10.1-254 \t ${PGreen}Active Host \n" 
+    printf "\t$0 -P ${PCyan}10.10.10.10 \t ${PGreen}Active Ports \n"
+}
+
+function def_validIp () {
+    local  ip=$1
+    local  stat=1
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255  && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+#Function hosting scanning
+function scanHost (){
+    local ipAddress=$1
+    local range=$2
+    local ipStart=$(echo "$ipAddress"| cut -d "-" -f 1| awk '{print $4}' FS=".")
+    local length=${#ipAddress}
+    local ipAddress=(${ipAddress::l-2})  
+    printf "${PPurple}[*] ${PGreen}Host Enumeration, ${POrange}Waiting\n"  
+    for ((i =$ipStart; i<$range+1; i++)); do
+        local ip="$ipAddress.$i"
+        timeout 1 bash -c "ping -c 1 $ip &>/dev/null" && printf "${PPurple} [✓] ${PBlue} $ip ${POrange} Host active \n" #& #threads
+        #wait
+    done
+}
+
+#Function Port scanning
+function scanPort (){
+    local ipAddress=$1
+    if def_validIp $ipAddress; then
+        printf "${PPurple}[*] ${PGreen}Port Enumeration    ${PBlue}$host:\n"
+        for ((port=0; port<=65535; port++)); do
+            if echo "s7v7n" 2>/dev/null > /dev/tcp/"$ipAddress"/"$port"
+            then
+                
+                printf "${PPurple} [✓] ${PBlue} $port ${POrange} Port Active\n"
+            fi
+        done
+    else
+        printf "${PRed}[X] Invalid IP address\n"
+        def_help
+        exit 1
+
+    fi
+
+}
+
+#Function main
+function def_main(){
+    if [[ $# -ne 2 ]]; then
+        printf "${PRed}[X] Error, wrong arguments\n";
+        def_help
+        exit 1;
+    else 
+        if [[ $1 = "-H" ]]; then
+            local ipAddress=$(echo $2| cut -d "-" -f 1)
+            local ip=$(echo $2| cut -d "-" -f 2)
+
+            if def_validIp $ipAddress; then
+                scanHost $ipAddress $ip
+            else 
+                printf "${PRed}[X] Invalid IP address\n"
+                def_help
+                exit 1
+            fi
+        fi
+        ##Scan Ports
+        if [[ $1 = "-P" ]]; then
+            local host=$2
+            scanPort $host
+        fi
+    fi
+}
+
+def_main $@
+
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # HostPortEnumerator
-Script in bash for host and port enumeration on networks, simplifying the identification of active services and their status on systems
+
+## Uso
+
+### Ayuda
+
+```shell
+[X] Invalid IP address
+[*] The necesary arguments are:
+        -H  : Host scanning
+        -P  : Port scanning
+         Example: 
+        ./hostPortDiscovery.sh -H 10.10.10.1-254         Active Host 
+        ./hostPortDiscovery.sh -P 10.10.10.10    Active Ports 
+```
+### Enumeración de Hosts
+
+```shell
+❯ ./hostPortDiscovery.sh -H 192.168.98.1-255
+[*] Host Enumeration, Waiting
+ [✓]  192.168.98.1  Host active 
+ [✓]  192.168.98.2  Host active 
+ [✓]  192.168.98.3  Host active 
+ [✓]  192.168.98.6  Host active 
+ [✓]  192.168.98.8  Host active
+```
+### Enumeración de Puertos
+
+```shell
+❯ ./hostPortDiscovery.sh -P 192.168.98.1
+[*] Port Enumeration    192.168.98.1:
+ [✓]  53  Port Active
+
+```
